32b0c1c8f5952b25ac0c91dcf5d501ca6073ff21
guilherme-hl1ma <guihlima@outlook.com>
2023-10-27 10:40:16 -0300

    initial modal mobile

---
+++ main.js
@@ -0,0 +1,12 @@
+const btnMenu = document.getElementById('btn-menu');
+const modalMobile = document.getElementById('modal-mobile');
+const closeModalMobile = document.getElementById('close-modal-mobile');
+
+btnMenu.addEventListener('click', function() {
+    modalMobile.style.display = 'block';
+    btnMenu.style.display = 'none';
+});
+
+closeModalMobile.addEventListener('click', function() {
+    modalMobile.style.display = 'none';
+});
